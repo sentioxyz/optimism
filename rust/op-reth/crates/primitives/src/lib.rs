a836e02f2e36776317bed41cc77dc12d94d1e72b
Anton Evangelatov <anton.evangelatov@oplabs.co>
2026-04-01 15:18:08 +0200

    fixup

--- rust/op-reth/crates/primitives/src/lib.rs
+++ rust/op-reth/crates/primitives/src/lib.rs
@@ -13,6 +13,8 @@ extern crate alloc;
 
 #[cfg(feature = "reth-codec")]
 use bytes as _;
+#[cfg(feature = "reth-codec")]
+use reth_codecs as _;
 #[cfg(feature = "serde-bincode-compat")]
 use reth_ethereum_primitives as _;
 #[cfg(feature = "reth-codec")]
